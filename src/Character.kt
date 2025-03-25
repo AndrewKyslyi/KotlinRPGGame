d79892bb1148548d6d48a2357acf1277ea51b26d
AndrewKyslyi <skorpypro@gmail.com>
2025-03-25 23:36:25 +0200

    part 1

---
+++ src/Character.kt
@@ -0,0 +1,12 @@
+class Character(
+    val hp: Int,
+    val mana: Int,
+    val resistance: Int,
+    val damage: Int,
+    val magResistance: Int,
+    val magDamage: Int,
+    var type: String?,
+    var attackSpeed: Float,
+) {
+
+}
---
+++ src/Main.kt
@@ -0,0 +1,14 @@
+fun main() {
+    println("Game started!!")
+
+    Thread.sleep(2500)
+    println("Choose an item:\n")
+
+    for (i in 1..5) {
+        val randomNumber = IntRange(1, 100)
+        // That's all for today!!
+        // C u in next Streams
+    }
+
+
+}
